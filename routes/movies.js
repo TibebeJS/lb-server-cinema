f6fd859e2e3bb502edfb1719287f387049e26fc5
TibebesJs <Tibebes.js@gmail.com>
2020-04-28 12:42:33 -0400

    fix some algorithm error

--- routes/movies.js
+++ routes/movies.js
@@ -12,10 +12,11 @@ const distinct = (array) => {
   const result = [];
   const map = new Map();
   for (const item of array) {
-    if (!map.has(item.id)) {
-      map.set(item.id, true); // set any value to Map
-      result.push(item);
-    }
+    if (item)
+      if (!map.has(item.id)) {
+        map.set(item.id, true); // set any value to Map
+        result.push(item);
+      }
   }
   return result;
 };
