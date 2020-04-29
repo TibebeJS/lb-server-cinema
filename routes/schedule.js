ad0c18521e39f596e6569ad96964fbbfed3551ae
TibebesJs <Tibebes.js@gmail.com>
2020-04-29 02:29:20 -0400

    remove dead code from schedule router

--- routes/schedule.js
+++ routes/schedule.js
@@ -2,50 +2,6 @@ const groupBy = require("lodash.groupby");
 const apiInstance = require("../tmdb-api");
 
 async function routes(fastify, options) {
-  // // returns all schedule for a given day
-  // fastify.route({
-  //   method: "GET",
-  //   url: "/",
-  //   schema: {
-  //     querystring: {
-  //       date: {
-  //         type: "string",
-  //       },
-  //     },
-  //     // response: {
-  //     //     200: {
-  //     //         type: 'array',
-  //     //         items: 'schedule#'
-  //     //     },
-  //     // }
-  //   },
-  //   preHandler: async (request, reply) => {},
-  //   handler: async (request, reply) => {
-  //     const { Schedule, Movie, Cinema, MovieType } = fastify.models;
-
-  //     // const schedules = await (Cinema.findAll({
-  //     //     include: [
-  //     //         // Movie,
-  //     //         Movie
-  //     //         // Cinema,
-  //     //         // MovieType
-  //     //     ],
-  //     //     where: {
-  //     //         Movies: {
-  //     //             Schedule: {
-  //     //                 id: 1
-  //     //             }
-  //     //         }
-  //     //     }
-  //     // }));
-
-  //     // console.log(schedules);
-  //     return await Schedule.findAll({
-  //       include: [Movie, Cinema, MovieType],
-  //       field: ["date", "time", "id"],
-  //     });
-  //   },
-  // });
 
   fastify.route({
     method: "GET",
@@ -67,23 +23,6 @@ async function routes(fastify, options) {
     handler: async (request, reply) => {
       const { Schedule, Movie, Cinema, MovieType } = fastify.models;
 
-      // const schedules = await (Cinema.findAll({
-      //     include: [
-      //         // Movie,
-      //         Movie
-      //         // Cinema,
-      //         // MovieType
-      //     ],
-      //     where: {
-      //         Movies: {
-      //             Schedule: {
-      //                 id: 1
-      //             }
-      //         }
-      //     }
-      // }));
-
-      // console.log(schedules);
       return Promise.all(
         Object.entries(
           groupBy(
@@ -163,8 +102,6 @@ async function routes(fastify, options) {
       const cinema = await Cinema.findByPk(request.body.cinemaId);
       const movieType = await MovieType.findByPk(request.body.movieTypeId);
 
-      // console.log(type)
-
       const schedule = await Schedule.create({
         date: request.body.date,
         time: request.body.time,
@@ -183,13 +120,13 @@ async function routes(fastify, options) {
             message,
             type,
             path,
-          })), // the http error message
+          })),
           message: err.errors.map(({ message, type, path }) => ({
             message,
             type,
             path,
-          })), // the user error message
-          statusCode: 400, // the http status code
+          })),
+          statusCode: 400
         };
       }
     },
