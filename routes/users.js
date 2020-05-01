eff92c6cdfce833b268ae9d9a18ab8fddf464329
TibebesJs <Tibebes.js@gmail.com>
2020-05-01 12:55:18 -0400

    implement delete and refactor the verification email

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const admin = require("../services/firebase-service");
-const emailService = require("../services/email-service");
+const { notification, verification } = require("../services/email-service");
 const path = require("path");
 
 const fs = require("fs");
@@ -9,12 +9,25 @@ const generateAdminEmailVerification = async (name, verificationLink) => {
   const template = await fsPromise.readFile(
     path.join(__dirname, "../templates/email/admin/verify-email.html"),
     {
-      encoding: 'utf-8'
+      encoding: "utf-8",
     }
   );
   return template
     .replace("{{emailVerificationLink}}", verificationLink)
     .replace("{{name}}", name);
+};
+
+const generateAdminDeleteNotification = async (user) => {
+  const template = await fsPromise.readFile(
+    path.join(
+      __dirname,
+      "../templates/email/admin/admin-access-revocation.html"
+    ),
+    {
+      encoding: "utf-8",
+    }
+  );
+  return template.replace("{{name}}", user.displayName);
 };
 
 async function routes(fastify, options) {
@@ -81,12 +94,49 @@ async function routes(fastify, options) {
         ),
       };
 
-      await new Promise((resolve, reject) => {
-        emailService.sendMail(mailOptions, function (err, info) {
-          if (err) reject(err);
-          else resolve(info);
+      try {
+        await new Promise((resolve, reject) => {
+          verification.sendMail(mailOptions, function (err, info) {
+            if (err) reject(err);
+            else resolve(info);
+          });
         });
-      });
+      } catch (error) {
+        console.log(error);
+      }
+      return {
+        success: true,
+      };
+    },
+  });
+
+  fastify.route({
+    method: "DELETE",
+    url: "/:uid",
+    schema: {},
+    preHandler: fastify.auth([fastify.verifyAdmin]),
+    handler: async (request, reply) => {
+      const user = await admin.auth().getUser(request.params.uid);
+
+      await admin.auth().deleteUser(request.params.uid);
+
+      const mailOptions = {
+        from: "notification@gast-cinema.addis-dev.com",
+        to: user.email,
+        subject: "GAST Cinema Admin Access revocation notification",
+        html: await generateAdminDeleteNotification(user),
+      };
+
+      try {
+        await new Promise((resolve, reject) => {
+          notification.sendMail(mailOptions, function (err, info) {
+            if (err) reject(err);
+            else resolve(info);
+          });
+        });
+      } catch (error) {
+        console.log(error);
+      }
 
       return {
         success: true,
