89bcfdf2e46622be8f7d6e3c1b29b3f7c740a367
TibebesJs <Tibebes.js@gmail.com>
2020-05-01 12:54:45 -0400

    make changes on the email service

--- services/email-service.js
+++ services/email-service.js
@@ -1,11 +1,22 @@
 const nodemailer = require("nodemailer");
 
-module.exports = nodemailer.createTransport({
-  host: "gast-cinema.addis-dev.com",
-  port: 465,
-  secure: true, // upgrade later with STARTTLS
-  auth: {
-    user: "verification@gast-cinema.addis-dev.com",
-    pass: "#dyDmRyZAz$G",
-  },
-});
+module.exports = {
+  verification: nodemailer.createTransport({
+    host: process.env.EMAIL_HOST_VERIFICATION,
+    port: process.env.EMAIL_PORT_VERIFICATION,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME_VERIFICATION,
+      pass: process.env.EMAIL_PASSWORD_VERIFICATION,
+    },
+  }),
+  notification: nodemailer.createTransport({
+    host: process.env.EMAIL_HOST_NOTIFICATION,
+    port: process.env.EMAIL_PORT_NOTIFICATION,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME_NOTIFICATION,
+      pass: process.env.EMAIL_PASSWORD_NOTIFICATION,
+    },
+  }),
+};
